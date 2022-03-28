f8bc2f5900248b275843936fd865939bf2cb8f6f
Lee McCormick <leemmccormick@gmail.com>
2022-03-28 20:30:00 +0100

    v0.5.6

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import nodeResolve from '@rollup/plugin-node-resolve';
 export default {
     input: 'dist/start.js',
     output: {
-        file: 'dist/bundle.js',
+        file: 'dist/zxgenerator.js',
         format: 'cjs',
     },
     plugins: [
